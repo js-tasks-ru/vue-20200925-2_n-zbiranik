996aaacb5a67ff6a8272546c7ca5bf59f9414e46
n.zbiranik <n.zbiranik@cdek.ru>
2020-10-02 20:39:39 +0700

    add solution for task 01-01

--- 01-basics/01-CounterButton/script.js
+++ 01-basics/01-CounterButton/script.js
@@ -1,4 +1,14 @@
 import Vue from './vue.esm.browser.js';
 
-// const app = ...
+const app = new Vue({
+  el: '#root',
+  data: {
+    message: 0,
+  },
+  methods: {
+    inc() {
+      this.message++
+    }
+  }
+});
 // Рекомендуется использовать МЕТОД в качестве обработчика события
